4d3b273f2bbfddb090bc13a9eecfb9ecb44a4bf6
kotori5210 <yukari5210am@gmail.com>
2026-06-26 22:18:37 +0900

    厳密等価演算子(===)を使用するように修正

--- sort.js
+++ sort.js
@@ -1,7 +1,12 @@
-var number =;
+var sortNumber = function (number) {
+  number.sort(function (a, b) {
+    if (a === b) {
+      return 0;
+    }
+    return a < b ? -1 : 1;
+  });
+};
 
-number.sort(function(a, b){
-  return a - b;
-});
-
+var number = [19, 3, 81, 1, 24, 21];
+sortNumber(number);
 console.log(number);
